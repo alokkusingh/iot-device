ab3492bd08cf20b77006a9f4598fd6ffe0dc7c47
alokkusingh <alok.ku.singh@gmail.com>
2025-06-29 11:21:55 +0530

    Added NTP Server

--- src/ESP32/MQTTClient/MQTTClient/MQTTClient.ino
+++ src/ESP32/MQTTClient/MQTTClient/MQTTClient.ino
@@ -14,15 +14,19 @@
 #define DTYPE DHT11  //Define DHT11 or DHT22 sensor type
 
 // WiFi and MQTT client initialization
-WiFiClientSecure esp_client;
-PubSubClient mqtt_client(esp_client);
+WiFiClientSecure espClient;
+PubSubClient mqttClient(espClient);
 
 DHT dht(DPIN, DTYPE);
 
 // Function Declarations
 void connectToWiFi();
+void configureNTPServer();
+void configureMQTTClient();
 void connectToMQTT();
 void mqttCallback(char *topic, byte *payload, unsigned int length);
+void senseAndPublishDHT11Telemetry();
+unsigned long getEpochTime();
 
 const char *clientId = "esp32-general-purpose-1";
 const char *statusTopic = "home/alok/status/esp32-general-purpose-1";
@@ -39,16 +43,12 @@ const unsigned long interval = 60000 * 60;  // Publish interval in milliseconds 
 
 void setup() {
   Serial.begin(115200);
+
   connectToWiFi();
 
-  // Set Root CA certificate
-  esp_client.setCACert(rootCACert);
-  esp_client.setCertificate(clientCert);
-  esp_client.setPrivateKey(clientKey);
+  configureNTPServer();
 
-  mqtt_client.setServer(MQTT_HOST, MQTT_PORT);
-  mqtt_client.setKeepAlive(60);
-  mqtt_client.setCallback(mqttCallback);
+  configureMQTTClient();
 
   connectToMQTT();
 
@@ -56,44 +56,28 @@ void setup() {
 }
 
 void loop() {
-  if (!mqtt_client.connected()) {
+  if (!mqttClient.connected()) {
     connectToMQTT();
   }
-  mqtt_client.loop();
+  mqttClient.loop();
 
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-
-    float tc = dht.readTemperature();      //Read temperature in C
-    float tf = dht.readTemperature(true);  //Read temperature in F
-    float hu = dht.readHumidity();
-
-    Serial.print("Temp C: ");
-    Serial.print(tc);
-    Serial.print("\n");
-
-    Serial.print("Temp F: ");
-    Serial.print(tf);
-    Serial.print("\n");
-
-    Serial.print("Humid: ");
-    Serial.print(dht.readHumidity());
-    Serial.print("%\n");
-
-    String str = "{\"deviceId\":\"";
-    String temperaturePayload = str + clientId + "\", \"epochTime\":" + getEpochTime() + ", \"temperature\":" + tc + ", \"unit\":\"Celsius\"}";
-    mqtt_client.publish(temperatureTopic, temperaturePayload.c_str());  // Publish message upon connection
-
-    String humidityPayload = str + clientId + "\", \"epochTime\":" + getEpochTime() + ", \"humidity\":" + hu + ", \"unit\":\"%\"}";
-    mqtt_client.publish(humidityTopic, humidityPayload.c_str());  // Publish message upon connection
+    senseAndPublishDHT11Telemetry();
   }
 }
 
-void printLocalTime() {
+void configureNTPServer() {
+  // Init and get the time
+  Serial.println("Connecting to NTP server...");
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) {
-    Serial.println("Failed to obtain time");
+    Serial.println("Failed to obtain time, lets try one more time!");
+    Serial.println("Connecting to NTP server...");
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
     return;
   }
   Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
@@ -113,30 +97,35 @@ void connectToWiFi() {
   }
   Serial.println("" + WiFi.localIP().toString());
   Serial.println("Connected to WiFi");
+}
 
-  // Init and get the time
-  Serial.println("Connecting to NTP server");
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
+void configureMQTTClient() {
+  // Set Root CA certificate
+  espClient.setCACert(rootCACert);
+  espClient.setCertificate(clientCert);
+  espClient.setPrivateKey(clientKey);
+
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  mqttClient.setKeepAlive(60);
+  mqttClient.setCallback(mqttCallback);
 }
 
 void connectToMQTT() {
-  while (!mqtt_client.connected()) {
+  while (!mqttClient.connected()) {
     Serial.printf("Connecting to MQTT Broker as %s...\n", clientId);
 
     String str = "{\"deviceId\":\"";
     String willPayload = str + clientId + "\", \"status\":\"offline\", \"epochTime\":" + getEpochTime() + ", \"ipAddress\":\"" + WiFi.localIP().toString() + "\"" + "}";
 
-    if (mqtt_client.connect(clientId, statusTopic, 0, true, willPayload.c_str())) {
+    if (mqttClient.connect(clientId, statusTopic, 0, true, willPayload.c_str())) {
       Serial.println("Connected to MQTT broker");
-      mqtt_client.subscribe(commandTopic);
 
       String connectedStatusPayload = str + clientId + "\", \"status\":\"online\", \"epochTime\":" + getEpochTime() + ", \"ipAddress\":\"" + WiFi.localIP().toString() + "\"" + "}";
 
-      mqtt_client.publish(statusTopic, connectedStatusPayload.c_str(), false);  // Publish message upon connection
+      mqttClient.publish(statusTopic, connectedStatusPayload.c_str(), false);  // Publish message upon connection
     } else {
       Serial.print("Failed to connect to MQTT broker, rc=");
-      Serial.print(mqtt_client.state());
+      Serial.print(mqttClient.state());
       Serial.println(" Retrying in 5 seconds.");
       delay(5000);
     }
@@ -151,4 +140,29 @@ void mqttCallback(char *topic, byte *payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println("\n-----------------------");
+}
+
+void senseAndPublishDHT11Telemetry() {
+  float tc = dht.readTemperature();      //Read temperature in C
+  float tf = dht.readTemperature(true);  //Read temperature in F
+  float hu = dht.readHumidity();
+
+  Serial.print("Temp C: ");
+  Serial.print(tc);
+  Serial.print("\n");
+
+  Serial.print("Temp F: ");
+  Serial.print(tf);
+  Serial.print("\n");
+
+  Serial.print("Humid: ");
+  Serial.print(dht.readHumidity());
+  Serial.print("%\n");
+
+  String str = "{\"deviceId\":\"";
+  String temperaturePayload = str + clientId + "\", \"epochTime\":" + getEpochTime() + ", \"temperature\":" + tc + ", \"unit\":\"Celsius\"}";
+  mqttClient.publish(temperatureTopic, temperaturePayload.c_str());  // Publish message upon connection
+
+  String humidityPayload = str + clientId + "\", \"epochTime\":" + getEpochTime() + ", \"humidity\":" + hu + ", \"unit\":\"%\"}";
+  mqttClient.publish(humidityTopic, humidityPayload.c_str());  // Publish message upon connection
 }
